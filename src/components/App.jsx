9101e12e0bd38ff4683222fae1ba831c55e2d8bb
Irina2304 <110673165+Irina2304@users.noreply.github.com>
2025-09-05 19:02:20 +0200

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,31 +1,16 @@
-import { AddForm } from './AddForm/AddForm';
-import { ContactsList } from './ContactsList/ContactsList';
-import { Filter } from './Filter/Filter';
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchAllContacts } from 'redux/operations';
-import { selectIsLoading } from 'redux/selectors';
-import { selectError } from 'redux/selectors';
-
 export const App = () => {
-  const dispatch = useDispatch();
-
-  const isLoading = useSelector(selectIsLoading);
-  const error = useSelector(selectError);
-
-  useEffect(() => {
-    dispatch(fetchAllContacts());
-  }, [dispatch]);
-
   return (
-    <div className="main-div">
-      {isLoading && <p>Loading tasks...</p>}
-      {error && <p>{error}</p>}
-      <h1>Phonebook</h1>
-      <AddForm />
-      <h2>Contacts</h2>
-      <Filter />
-      <ContactsList />
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}
+    >
+      React homework template
     </div>
   );
 };
